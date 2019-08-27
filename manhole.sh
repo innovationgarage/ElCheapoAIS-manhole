d8d440581b8d2f97af2b8d3c9370efde54deed31
Egil Moeller <egil@innovationgarage.no>
2019-08-27 14:17:55 +0000

    Bugfixes for openwrt

--- manhole.sh
+++ manhole.sh
@@ -13,11 +13,11 @@ source ./manhole-ordering.txt
 
 curl -s -o ./manhole-script -D ./manhole-headers "${manholeurl}/${stationid}"
 status="$?"
-ordering="$(grep Ordering ./manhole-header | sed -e "s+.*: *++g")"
+ordering="$(grep Ordering ./manhole-headers | sed -e "s+.*: *++g" | tr -d "\r")"
 
 if [ "$status" == "0" ] && ((ordering > last_ordering)); then
     echo "Workman is entering the manhole for the ${ordering}th time..."
-    now="$(date --iso-8601=seconds)"
+    now="$(date -Iseconds)"
     chmod ugo+x ./manhole-script
     ./manhole-script > ./manhole-log.txt 2>&1
     curl -s -X PUT --data-binary @./manhole-log.txt "${manholeurl}/${stationid}/${ordering}"
