646625ae4cba70e1500c1df8ed5d532ffc0e823a
Egil Moeller <egil@innovationgarage.no>
2019-08-27 13:15:21 +0000

    Use curl instead of wget

--- manhole.sh
+++ manhole.sh
@@ -11,17 +11,16 @@ EOF
 fi
 source ./manhole-ordering.txt
 
-wget --quiet --server-response -O ./manhole-script "${manholeurl}/${stationid}" > ./manhole-headers.txt 2>&1
+curl -s -o ./manhole-script -D ./manhole-headers "${manholeurl}/${stationid}"
 status="$?"
-ordering="$(grep Ordering ./manhole-headers.txt | sed -e "s+.*: *++g")"
-
+ordering="$(grep Ordering ./manhole-header | sed -e "s+.*: *++g")"
 
 if [ "$status" == "0" ] && ((ordering > last_ordering)); then
     echo "Workman is entering the manhole for the ${ordering}th time..."
     now="$(date --iso-8601=seconds)"
     chmod ugo+x ./manhole-script
     ./manhole-script > ./manhole-log.txt 2>&1
-    wget --quiet -O - --method=PUT --body-file=./manhole-log.txt "${manholeurl}/${stationid}/${ordering}"
+    curl -s -X PUT --data-binary @./manhole-log.txt "${manholeurl}/${stationid}/${ordering}"
 
     cat > ./manhole-ordering.txt <<EOF
 last_ordering="$ordering"
